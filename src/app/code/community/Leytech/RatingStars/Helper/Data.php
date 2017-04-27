dc93a68aec320461e7d60e63f4bd1ddbad242ace
Chris Nolan <chris@leytech.co.uk>
2017-04-27 21:01:16 +0100

    Started to build module

---
+++ src/app/code/community/Leytech/RatingStars/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    Leytech_RatingStars
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
+ */
+
+class Leytech_RatingStars_Helper_Data extends Mage_Core_Helper_Data
+{
+
+}
