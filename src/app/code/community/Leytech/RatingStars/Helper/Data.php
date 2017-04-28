4aeff130fc9a29dc6633f65c52a192de4a1b5d0a
Chris Nolan <chris@leytech.co.uk>
2017-04-28 11:21:02 +0100

    Layout changes for base and rwd

--- src/app/code/community/Leytech/RatingStars/Helper/Data.php
+++ src/app/code/community/Leytech/RatingStars/Helper/Data.php
@@ -9,4 +9,25 @@
 class Leytech_RatingStars_Helper_Data extends Mage_Core_Helper_Data
 {
 
+    const XML_PATH_SETTINGS_ENABLED = 'leytech_ratingstars/settings/enabled';
+    const XML_PATH_SETTINGS_INCLUDE_JQUERY = 'leytech_ratingstars/settings/include_jquery';
+
+    public function isEnabled()
+    {
+        return Mage::getStoreConfig(self::XML_PATH_SETTINGS_ENABLED);
+    }
+
+    public function getIncludeJquery()
+    {
+        return Mage::getStoreConfig(self::XML_PATH_SETTINGS_INCLUDE_JQUERY);
+    }
+
+    public function switchTemplate()
+    {
+        if(!$this->isEnabled()) {
+            return 'review/form.phtml';
+        }
+        return 'leytech/ratingstars/form.phtml';
+    }
+
 }
